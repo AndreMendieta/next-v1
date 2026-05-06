01f42abad7fe940ebfccad3d976e5ca535412d5c
AndreMendieta <cristianmendieta2203@gmail.com>
2026-05-06 02:19:13 +0000

    commit 05

---
+++ app/dos/page.tsx
@@ -0,0 +1,12 @@
+import Hijo from "./hijo";
+
+export default function Home() {
+  return (
+    <div>
+      <p>Dos</p>
+      <Hijo>
+        Ocaiman
+      </Hijo>
+    </div>
+  )
+}
---
+++ app/uno/page.tsx
@@ -0,0 +1,12 @@
+import Props from "./props";
+
+export default function Home() {
+    
+  return (
+    
+    <div>
+      <p>Uno</p>
+      <Props nombre="este es un props"/>
+    </div>
+  )
+}
